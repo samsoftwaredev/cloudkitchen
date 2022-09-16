29b432b10182f6220f42e0e28f333883252edcd3
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2022-09-16 02:20:59 -0500

    2 decimal places

--- utils/currency.ts
+++ utils/currency.ts
@@ -1,9 +1,11 @@
-export const currencyUSDCentsFormatter = (cents: number): number => cents / 100;
+export const currencyUSDCentsFormatter = (cents: number): string =>
+  (cents / 100).toFixed(2);
 
-export const addComma = (num: number): string =>
+export const addComma = (num: number | string): string =>
   num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-export const USDFormatter = (dollar: number): string => `$${addComma(dollar)}`;
+export const USDFormatter = (dollar: number | string): string =>
+  `$${addComma(dollar)}`;
 
 export const centsToUSD = (cents: number): string =>
   USDFormatter(currencyUSDCentsFormatter(cents));
